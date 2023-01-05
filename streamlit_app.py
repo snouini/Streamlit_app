925cc7f162e6de35331421930ff8065651adda73
snouini <98923995+snouini@users.noreply.github.com>
2023-01-05 20:00:00 +0100

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ streamlit.header('Fruity Vice Fruit Advice!')
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
-fruit_choice = lower(fruit_choice)
+fruit_choice = fruit_choice.lower()
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/{fruit_choice}")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
