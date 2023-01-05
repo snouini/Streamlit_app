71815fe43fc6ad1580f18262bb5c56aeb26f073d
snouini <98923995+snouini@users.noreply.github.com>
2023-01-05 20:41:25 +0100

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruity Vice Fruit Advice!')
 
 def get_fruityvice_data(this_fruit_choice):
-  fruit_choice = fruit_choice.lower()
+  this_fruit_choice = this_fruit_choice.lower()
   fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
   fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
   return fruityvice_normalized
